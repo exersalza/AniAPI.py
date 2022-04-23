5af75fc4fbc3367277b2252666541a3845cec92d
exersalza <squidodle44@gmail.com>
2022-04-23 17:39:22 +0200

    Updated __repr__ method

--- convertor.py
+++ convertor.py
@@ -1,19 +1,9 @@
-from config import API_TOKEN
-from type.anime import Anime
-from wrapper import AniApi
+from _types.anime import Anime
 
 
-class Dict2Obj(Anime):
+class Dict2AnimeObj(Anime):
     def __init__(self, **dict_):
         self.__dict__ = dict_
 
     def __repr__(self):
-        return str(self.__dict__)
-
-
-if __name__ == '__main__':
-    api = AniApi(token=API_TOKEN)
-    d = api.get_anime(1).get('data')
-    print(d)
-    d2o: Anime = Dict2Obj(**d)
-    print(d2o.titles)
+        return f'<id={self.id} title={(list(self.titles.values())[0])!r} episodes={self.episodes_count} status={self.status}>'
